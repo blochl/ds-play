cc1b06ed1ad1db3bd72be9e824e038d449f6ea78
Leonid Bloch <lb.workbox@gmail.com>
2020-07-10 08:59:39 +0300

    Avoid code duplication when creating dummy node on init
    Signed-off-by: Leonid Bloch <lb.workbox@gmail.com>

--- ds-play.c
+++ ds-play.c
@@ -13,42 +13,6 @@ typedef struct node_t {
 static node_t* pool;
 static uint32_t node_count;
 
-int init_pool() {
-    if (pool) {
-        /* The pool is already initiated */
-        return -1;
-    }
-
-    /* A dummy node for enabling the removal of "actual" nodes
-     * (which must not be the last one). node_count is not increased for it. */
-    pool = (node_t*)malloc(sizeof(node_t));
-    if (!pool) {
-        return -1;  /* error */
-    }
-    pool->s = (void*)((node_t**)malloc(MEMBER_SIZE + sizeof(node_t*)) + 1);
-    if (!((node_t**)pool->s - 1)) {
-        return -1;  /* error */
-    }
-    *((node_t**)pool->s - 1) = pool;
-    pool->next = NULL;
-
-    return 0;
-}
-
-int free_pool() {
-    node_t* tmp = NULL;
-
-    while (pool) {
-        free((node_t**)pool->s - 1);
-        tmp = pool;
-        pool = pool->next;
-        free(tmp);
-    }
-    node_count = 0;
-
-    return 0;
-}
-
 void* add_node() {
     node_t* tmp = NULL;
 
@@ -68,6 +32,34 @@ void* add_node() {
     node_count++;
 
     return pool->s;
+}
+
+int init_pool() {
+    if (pool) {
+        /* The pool is already initiated */
+        return -1;
+    }
+
+    /* A dummy node for enabling the removal of "actual" nodes
+     * (which must not be the last one). node_count is not increased for it. */
+    add_node();
+    node_count = 0;
+
+    return 0;
+}
+
+int free_pool() {
+    node_t* tmp = NULL;
+
+    while (pool) {
+        free((node_t**)pool->s - 1);
+        tmp = pool;
+        pool = pool->next;
+        free(tmp);
+    }
+    node_count = 0;
+
+    return 0;
 }
 
 void rm_node(void* x) {
